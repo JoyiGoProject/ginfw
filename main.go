f8685a02e9b49ecf2929310753291232015da5e1
Joyi <Joyi@joyi.name>
2017-12-05 13:44:22 +0800

    Update

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 /* func main() {
@@ -107,7 +108,7 @@ func DeleteHandler(c *gin.Context) {
 }
 */
 
-func main() {
+/* func main() {
 	//获取一个路由handler
 	router := gin.Default()
 
@@ -121,4 +122,100 @@ func main() {
 		c.String(http.StatusOK, name+" is "+action)
 	})
 	router.Run(":8000")
+}
+*/
+
+//极简实例
+/* func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+} */
+
+//构建restful api
+func main() {
+	//获取路由实例
+	router := gin.Default()
+	gin.SetMode(gin.DebugMode)
+	// gin.SetMode(gin.ReleaseMode)
+	v1 := router.Group("/api/v1/userinfo")
+	{
+		v1.POST("/", CreateUser)
+		v1.GET("/", FetchAllUsers)
+		v1.GET("/:id", FetchSingleUser)
+		v1.PUT("/:id", UpdateUser)
+		v1.DELETE("/:id", DeleteUser)
+	}
+
+	router.Run()
+}
+
+type Person struct {
+	Id   int64
+	Name string
+	Age  int64
+}
+
+//查询单个用户
+func FetchSingleUser(c *gin.Context) {
+	id := c.Param("id")
+	db, err := sql.Open("mysql", "root:admin@/test?charset=utf8")
+	checkErr(err)
+
+	defer db.Close()
+
+	err = db.Ping()
+	checkErr(err)
+
+	var (
+		person Person
+		result gin.H
+	)
+
+	row := db.QueryRow("select * from user where id = ?;", id)
+	row.Scan(&person.Id, &person.Name, &person.Age)
+	if err != nil {
+		checkErr(err)
+		//If no results send null
+		result = gin.H{
+			"result": nil,
+			"count":  0,
+		}
+	} else {
+		result = gin.H{
+			"result": person,
+			"count":  1,
+		}
+	}
+	c.JSON(http.StatusOK, result)
+}
+
+//创建用户
+func CreateUser(c *gin.Context) {
+
+}
+
+//查询所有用户
+func FetchAllUsers(c *gin.Context) {
+
+}
+
+//更新用户
+func UpdateUser(c *gin.Context) {
+
+}
+
+//删除用户
+func DeleteUser(c *gin.Context) {
+
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
