42cc459cfe2d381b2f3f1dc4faa3acca332b6708
Joyi <Joyi@joyi.name>
2017-12-12 15:52:19 +0800

    Update the index

---
+++ code/gin1/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	gin.SetMode(gin.DebugMode) //全局设置环境，此为开发环境，线上环境为gin.ReleaseMode
+	router := gin.Default()    //获得路由实例
+
+	//添加中间件
+	router.Use(Middleware)
+	//注册接口
+	router.GET("/simple/server/get", GetHandler)
+	router.POST("/simple/server/post", PostHandler)
+	router.PUT("/simple/server/put", PutHandler)
+	router.DELETE("/simple/server/delete", DeleteHandler)
+	router.GET("/topage", Index)
+	//监听端口
+	http.ListenAndServe(":4567", router)
+}
+
+func Middleware(c *gin.Context) {
+	fmt.Println("this is a middleware!")
+}
+
+func GetHandler(c *gin.Context) {
+	value, exist := c.GetQuery("key")
+	if !exist {
+		value = "the key is not exist!"
+	}
+	c.Data(http.StatusOK, "text/plain", []byte(fmt.Sprintf("get success! %s\n", value)))
+	return
+}
+func PostHandler(c *gin.Context) {
+	type JsonHolder struct {
+		Id   int    `json:"id"`
+		Name string `json:"name"`
+	}
+	holder := JsonHolder{Id: 1, Name: "my name"}
+	//若返回json数据，可以直接使用gin封装好的JSON方法
+	c.JSON(http.StatusOK, holder)
+	return
+}
+func PutHandler(c *gin.Context) {
+	c.Data(http.StatusOK, "text/plain", []byte("put success!\n"))
+	return
+}
+func DeleteHandler(c *gin.Context) {
+	c.Data(http.StatusOK, "text/plain", []byte("delete success!\n"))
+	return
+}
+
+func Index(c *gin.Context) {
+	ipaddr := c.ClientIP()
+	c.Abort()
+	ctype := c.ContentType()
+	fmt.Println("IP地址是：", ipaddr)
+	fmt.Println("内容的类型是：", ctype)
+	hheheh := c.Param(ipaddr)
+	fmt.Println(hheheh)
+	i, err := c.Writer.WriteString("这是您的IP地址：\n")
+	if err != nil {
+		fmt.Println("-----", i, err)
+		return
+	}
+	fmt.Println("返回的结果是：", i)
+	c.Data(http.StatusOK, "text/plain", []byte(fmt.Sprintf("\t%s\n", ipaddr)))
+}
